f9153e5dd454019a53a4b5d8652e30177e208b80
Evan Keane <evankeane0@icloud.com>
2025-03-25 14:16:38 -0500

    Added carousel

---
+++ assignment-5/site.js
@@ -0,0 +1,58 @@
+console.log('Test Console')
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+
+const welcome = document.querySelector('#welcome')
+if (isMorning) welcome.textContent = 'Good Morning!'
+if (isAfternoon) welcome.textContent = 'Good Afternoon!'
+if (isEvening) welcome.textContent = 'Good Evening!'
+
+localStorage.setItem("It's a secret to everybody.", 'Test message')
+
+
+
+
+
+
+
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prev = document.querySelector('#buttons>#prev')
+const next = document.querySelector('#buttons>#next')
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
+prev.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+next.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
